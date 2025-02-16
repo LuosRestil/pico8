06b5062fed25e92f9a0d06cf9bd69639ec4fecf5
LuosRestil <briansmithwebdeveloper@gmail.com>
2025-02-16 15:45:19 -0500

    rudimentary sfx

---
+++ extern/main.lua
@@ -0,0 +1,14 @@
+my_val = 20
+
+-- function _init()
+-- end
+
+-- function _update()
+--   x=64+sin(t())*16
+-- end
+
+-- function _draw()
+--   cls()
+--   circfill(x,64,24,7)
+--   draw_player()
+-- end
---
+++ extern/player/draw.lua
@@ -0,0 +1,4 @@
+function do_it()
+  x = 64 + sin(t()) * 16
+  circfill(x, 64, 20, 7)
+end
